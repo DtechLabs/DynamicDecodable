a53dfe42348b4ce9cc98429b005b02f2e6c7a1f6
Yuri Drigin <yuri.drigin@icloud.com>
2024-06-10 12:23:35 +0400

    Implemented DynamicDecodeMapping Plugin

--- Package.swift
+++ Package.swift
@@ -6,38 +6,41 @@ import CompilerPluginSupport
 
 let package = Package(
     name: "DynamicDecodable",
-    platforms: [.macOS(.v12), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DynamicDecodable",
             targets: ["DynamicDecodable"]
-        ),
-        .library(name: "DynamicMapping", targets: ["DynamicMapping"])
+        )
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
     ],
     targets: [
         .macro(
-            name: "DynamicMappingMacroImpl",
+            name: "DynamicDecodeMappingMacros",
             dependencies: [
                 .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
             ]
         ),
-        .target(name: "DynamicDecodable"),
         .target(
-            name: "DynamicMapping",
-            dependencies: ["DynamicMappingMacroImpl"]
+            name: "DynamicDecodable",
+            plugins: [.plugin(name: "DynamicDecodeMappingMacros")]
         ),
         .testTarget(
             name: "DynamicDecodableTests",
             dependencies: [
-                "DynamicMapping",
-                "DynamicDecodable",
-                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax")
+                "DynamicDecodable"
             ]
+        ),
+        .testTarget(
+            name: "DynamicDecodeMappingTests",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
+            ],
+            plugins: [.plugin(name: "DynamicDecodeMappingMacros")]
         )
     ]
 )
---
+++ Sources/DynamicDecodable/DynamicDecodeMappable.swift
@@ -0,0 +1,20 @@
+//
+//  DynamicDecodeMappable.swift
+//  DynamicDecodable
+//
+//  Created by Yury Dryhin on 31.05.2024.
+//  Copyright © 2024 . All rights reserved.
+//
+//  This software is released under the MIT License.
+//
+
+import Foundation
+
+public protocol DynamicDecodeMappable {
+    
+    init?(_ data: DynamicDecodable?)
+    
+}
+
+@attached(extension, conformances: DynamicDecodeMappable)
+public macro DynamicDecodableMapping() = #externalMacro(module: "DynamicDecodeMappingMacros", type: "DynamicDecodeMappingMacro")
